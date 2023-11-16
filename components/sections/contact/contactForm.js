303a9468bcb1d7431d7a72fc58210f46edd6c738
Sergio Garcia Gallego <gallegogarciasergio@gmail.com>
2023-11-16 23:38:42 +0000

    changing form endpoint

--- components/sections/contact/contactForm.js
+++ components/sections/contact/contactForm.js
@@ -1,30 +1,36 @@
 import styles from "./contactForm.module.scss"
 
 export default function ContactForm() {
-  const formData = {
-    inputOne: "938283395",
-    inputTwo: "849173883",
-    inputThree: "1261115226",
-    inputFour: "803606353",
-    inputFive: "1747769461",
-    inputSix: "1231672355",
-  }
 
   return (
     <form
-      id="form"
       className="container"
       method="post" 
-      action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeqiTxoYcoJcvjw_3tsxU5OBLBzsAJ4ggXDivGm4b35Jtz9Lg/formResponse"
+      action="https://formsubmit.co/hello@originblank.com"
     >
+      <input 
+        type="hidden" 
+        name="_next" 
+        value="https://originblank.com/"
+      />
+      <input 
+        type="text" 
+        name="_honey" 
+        style={{display: "none"}}
+      />
+      <input 
+        type="hidden" 
+        name="_template" 
+        value="table"
+      />
       <div className={styles.wrapper}>
         <div className={styles.field}>
           <label htmlFor="inputOne">Your name</label>
           <input 
-            placeholder="John Doe"
-            type="text"
-            name={`entry.${formData.inputOne}`}
             id="inputOne"
+            type="text"
+            name="name"
+            placeholder="John Doe"
             required
           />
         </div>
@@ -32,20 +38,20 @@ export default function ContactForm() {
           <div className={styles.field}>
             <label htmlFor="inputTwo">Your email</label>
             <input 
-              placeholder="john@brilliantbrand.com"
-              type="text"
-              name={`entry.${formData.inputTwo}`}
               id="inputTwo"
+              type="email"
+              name="email"
+              placeholder="john@brilliantbrand.com"
               required
             />
           </div>
           <div className={styles.field}>
             <label htmlFor="inputThree">Phone number</label>
             <input 
-              placeholder="+44 0208 1234567"
-              type="text"
-              name={`entry.${formData.inputThree}`}
               id="inputThree"
+              type="tel"
+              name="phoneNumber"
+              placeholder="+44 0208 1234567"
             />
           </div>
         </div>
@@ -53,29 +59,29 @@ export default function ContactForm() {
           <div className={styles.field}>
             <label htmlFor="inputFour">Company Name</label>
             <input 
-              placeholder="Brilliant Brand"
-              type="text"
-              name={`entry.${formData.inputFour}`}
               id="inputFour"
+              type="text"
+              name="companyName"
+              placeholder="Brilliant Brand"
             />
           </div>
           <div className={styles.field}>
             <label htmlFor="inputFive">Website</label>
             <input 
-              placeholder="brilliantbrand.com"
-              type="text"
-              name={`entry.${formData.inputFive}`}
               id="inputFive"
+              type="text"
+              name="website"
+              placeholder="brilliantbrand.com"
             />
           </div>
         </div>
         <div className={styles.field}>
           <label htmlFor="inputSix">Message</label>
           <textarea 
-            placeholder="Tell us a little about your brand and why you're reaching out."
-            rows="8"
-            name={`entry.${formData.inputSix}`}
             id="inputSix" 
+            name="message"
+            rows="8"
+            placeholder="Tell us a little about your brand and why you're reaching out."
             required
           ></textarea>
         </div>
