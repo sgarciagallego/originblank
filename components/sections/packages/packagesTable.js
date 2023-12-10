42211c1970adbfeab01b3c61130b0989acb2b37f
Sergio Garcia Gallego <gallegogarciasergio@gmail.com>
2023-12-10 01:27:14 +0000

    making table variations for responsiveness *not complete*

--- components/sections/packages/packagesTable.js
+++ components/sections/packages/packagesTable.js
@@ -1,5 +1,8 @@
-import Vector from "@/components/atoms/vector"
+'use client'
+
+import React, { useState, useEffect } from "react";
 import styles from "./packagesTable.module.scss"
+import Vector from "@/components/atoms/vector"
 import CtaLight from "@/components/atoms/cta/ctaLight"
 
 const packages = [
@@ -81,6 +84,27 @@ const formatServiceType = (service) => {
 }
 
 export default function PackagesTable() {
+  const [viewportWidth, setViewportWidth] = useState(0)
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false)
+
+  useEffect(() => {
+    const updateViewportWidth = () => {
+      setViewportWidth(window.innerWidth)
+    }
+
+    updateViewportWidth()
+
+    window.addEventListener("resize", updateViewportWidth)
+
+    return () => {
+      window.removeEventListener("resize", updateViewportWidth)
+    }
+  })
+
+  const toggleMobileMenu = () => {
+    setMobileMenuVisible(!mobileMenuVisible)
+  }
+
   const services = [
     "duration",
     "values",
@@ -96,58 +120,117 @@ export default function PackagesTable() {
 
   return (
     <section className={`space ${styles.wrapper}`}>
-      <table className={styles.packages}>
-        <caption className="screenReaderVisible">
-          A table
-        </caption>
-        <tbody>
-          <tr>
-            <th>
-              <h2>Not sure which package is right for you?</h2>
-              <p>We respect your time, and we know it&apos;s valuable. While our rates are competitive, we get that it might not suit every budget. Being transparent helps us save time for everyone.</p>
-            </th>
-            {packages.map(cell => (
-              <td key={cell.id}>
-                <h2>{cell.heading}</h2>
-                <p>{cell.desc}</p>
-                <div className={styles.priceTag}>
-                  <span className={styles.currency}>£</span>
-                  {cell.price}
-                </div>
-                <CtaLight href="/contact">Enquire now</CtaLight>
-              </td>
-            ))}
-          </tr>
-          {services.map((service, index) => (
-            <tr key={index}>
-              <th>
-                {formatServiceType(service)}
+      {viewportWidth < 768 ? (
+        <></>
+      ) : viewportWidth < 992 ? (
+        <table className={styles.tabletPackages}>
+          <caption className="screenReaderVisible">
+            A table
+          </caption>
+          <tbody>
+            <tr>
+              <th colSpan={4}>
+                <h2>Not sure which package is right for you?</h2>
+                <p>We respect your time, and we know it&apos;s valuable. While our rates are competitive, we get that it might not suit every budget. Being transparent helps us save time for everyone.</p>
               </th>
+            </tr>
+            <tr>
               {packages.map(cell => (
                 <td key={cell.id}>
-                  {cell.hasOwnProperty("extraCharges") && service === "extraCharges" ? (
-                    cell.extraCharges !== "" ? (
-                      cell.extraCharges
-                    ) : (
-                      <Vector name="minus" />
-                    )
-                  ) : cell.hasOwnProperty("duration") && service === "duration" ? (
-                    cell.duration !== "" ? (
-                      `${cell.duration} ${cell.duration === 1 ? "week" : "weeks"}`
-                    ) : (
-                      <Vector name="uncheck" />
-                    )
-                  ) : cell[service] ? (
-                    <Vector name="check" />
-                  ) : (
-                    <Vector name="uncheck" />
-                  )}
+                  <h2>{cell.heading}</h2>
+                  <p>{cell.desc}</p>
+                  <div className={styles.priceTag}>
+                    <span className={styles.currency}>£</span>
+                    {cell.price}
+                  </div>
+                  <CtaLight href="/contact">Enquire</CtaLight>
                 </td>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+            {services.map((service, index) => (
+              <React.Fragment key={index}>
+                <tr>
+                  <th colSpan={4}>{formatServiceType(service)}</th>
+                </tr>
+                <tr>
+                  {packages.map(cell => (
+                    <td key={cell.id}>
+                      {cell.hasOwnProperty("extraCharges") && service === "extraCharges" ? (
+                        cell.extraCharges !== "" ? (
+                          cell.extraCharges
+                        ) : (
+                          <Vector name="minus" />
+                        )
+                      ) : cell.hasOwnProperty("duration") && service === "duration" ? (
+                        cell.duration !== "" ? (
+                          `${cell.duration} ${cell.duration === 1 ? "week" : "weeks"}`
+                        ) : (
+                          <Vector name="uncheck" />
+                        )
+                      ) : cell[service] ? (
+                        <Vector name="check" />
+                      ) : (
+                        <Vector name="uncheck" />
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <table className={styles.packages}>
+          <caption className="screenReaderVisible">
+            A table
+          </caption>
+          <tbody>
+            <tr>
+              <th>
+                <h2>Not sure which package is right for you?</h2>
+                <p>We respect your time, and we know it&apos;s valuable. While our rates are competitive, we get that it might not suit every budget. Being transparent helps us save time for everyone.</p>
+              </th>
+              {packages.map(cell => (
+                <td key={cell.id}>
+                  <h2>{cell.heading}</h2>
+                  <p>{cell.desc}</p>
+                  <div className={styles.priceTag}>
+                    <span className={styles.currency}>£</span>
+                    {cell.price}
+                  </div>
+                  <CtaLight href="/contact">Enquire</CtaLight>
+                </td>
+              ))}
+            </tr>
+            {services.map((service, index) => (
+              <tr key={index}>
+                <th>{formatServiceType(service)}</th>
+                {packages.map(cell => (
+                  <td key={cell.id}>
+                    {cell.hasOwnProperty("extraCharges") && service === "extraCharges" ? (
+                      cell.extraCharges !== "" ? (
+                        cell.extraCharges
+                      ) : (
+                        <Vector name="minus" />
+                      )
+                    ) : cell.hasOwnProperty("duration") && service === "duration" ? (
+                      cell.duration !== "" ? (
+                        `${cell.duration} ${cell.duration === 1 ? "week" : "weeks"}`
+                      ) : (
+                        <Vector name="uncheck" />
+                      )
+                    ) : cell[service] ? (
+                      <Vector name="check" />
+                    ) : (
+                      <Vector name="uncheck" />
+                    )}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </section>
   )
 }
